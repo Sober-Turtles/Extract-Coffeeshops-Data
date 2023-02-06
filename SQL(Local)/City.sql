b1c1d47937578483d8743c9a8d8f91fcc6f8338a
dibaaminshahidi <dibaaminshahidi@gmail.com>
2023-02-06 22:39:30 +0330

    SQL files for making local datbase(Please make a local db and then run these files(Note that make sure you run city,cafe and features respectively at first

---
+++ SQL(Local)/City.sql
@@ -0,0 +1,37 @@
+-- Database export via SQLPro (https://www.sqlprostudio.com/allapps.html)
+-- Exported by dibaaminshahidi at 17-11-1401 10:32 PM.
+-- WARNING: This file may contain descructive statements such as DROPs.
+-- Please ensure that you are running the script at the proper location.
+
+
+-- BEGIN TABLE City
+DROP TABLE IF EXISTS City;
+CREATE TABLE `City` (
+  `id` int NOT NULL,
+  `name` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Inserting 17 rows into City
+-- Insert batch #1
+INSERT INTO City (id, name) VALUES
+(0, 'tabriz'),
+(1, 'zanjan'),
+(2, 'urmia'),
+(3, 'hamedan'),
+(4, 'isfahan'),
+(5, 'kish'),
+(6, 'kerman'),
+(7, 'karaj'),
+(8, 'bandarabbas'),
+(9, 'sabzevar'),
+(10, 'mashhad'),
+(11, 'shiraz'),
+(12, 'ghom'),
+(13, 'ahwaz'),
+(14, 'arak'),
+(15, 'qazvin'),
+(16, 'tehran');
+
+-- END TABLE City
+
---
+++ SQL(Local)/Features.sql
@@ -0,0 +1,32 @@
+-- Database export via SQLPro (https://www.sqlprostudio.com/allapps.html)
+-- Exported by dibaaminshahidi at 17-11-1401 10:32 PM.
+-- WARNING: This file may contain descructive statements such as DROPs.
+-- Please ensure that you are running the script at the proper location.
+
+
+-- BEGIN TABLE Features
+DROP TABLE IF EXISTS Features;
+CREATE TABLE `Features` (
+  `id` int NOT NULL,
+  `feature_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Inserting 12 rows into Features
+-- Insert batch #1
+INSERT INTO Features (id, feature_name) VALUES
+(0, 'چشم انداز'),
+(1, 'محل بازی کودکان'),
+(2, 'صبحانه'),
+(3, 'موسیقی زنده'),
+(4, 'ارسال رایگان (Delivery)'),
+(5, 'اینترنت رایگان'),
+(6, 'سالاد بار'),
+(7, 'سیگار'),
+(8, 'قلیان'),
+(9, 'دستگاه کارت خوان'),
+(10, 'فضای باز'),
+(11, 'پارکینگ');
+
+-- END TABLE Features
+
